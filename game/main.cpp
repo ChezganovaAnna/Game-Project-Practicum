8bb877df2779f51e3a3177de73e95dbc0d2d16f6
ChezganovaAnna <tchezganova.aniuta@yandex.ru>
2023-11-12 16:52:21 +0300

    2 hours

--- game/main.cpp
+++ game/main.cpp
@@ -1,22 +1,58 @@
 #include <SFML/Graphics.hpp>
 
+class Game
+{
+  public:
+    Game();
+    void run();
+
+  private:
+    void ProcessEvents();
+    void update();
+    void render();
+
+  private:
+    sf::RenderWindow mWindow;
+    sf::CircleShape mPlayer;
+}
+
+Game::Game()
+    : mWindow(sf::VideoMode(640, 480), "SFML Application"), mPlayer()
+{
+    mPlayer.setRadius(40.f);
+    mPlayer.setPosition(100.f, 100.f);
+    mPlyer.SetFillColor(sf::Color::Cyan);
+}
+
+void Game::run()
+{
+    while (mWindow.isOpen())
+    {
+        ProcessEvents();
+        update();
+        render();
+    }
+}
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Application");
-    sf::CircleShape shape;
-    shape.setRadius(40.f);
-    shape.setPosition(100.f, 100.f);
-    shape.SetFillColor(sf::Color::Cyan);
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == = sf::Event::Closed)
-                window.close();
-        }
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+    Game game;
+    game.run();
+    // sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Application");
+    // sf::CircleShape shape;
+    // shape.setRadius(40.f);
+    // shape.setPosition(100.f, 100.f);
+    // shape.SetFillColor(sf::Color::Cyan);
+    // while (window.isOpen())
+    // {
+    //     sf::Event event;
+    //     while (window.pollEvent(event))
+    //     {
+    //         if (event.type == = sf::Event::Closed)
+    //             window.close();
+    //     }
+    //     window.clear();
+    //     window.draw(shape);
+    //     window.display();
+    // }
 }
